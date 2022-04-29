d6bf4611c1db1d8f267b7f9ef0318256a0161bd9
Draganek <100842378+Draganek@users.noreply.github.com>
2022-04-30 01:40:48 +0200

    Add files via upload

---
+++ menu_customer.py
@@ -0,0 +1,33 @@
+
+def get_menu():
+    menu = [
+        
+        #Shots
+        ["Kamikadze (3szt.)",["-Wódka","-Likier blue Curacao"],["-Sok z cytryny"],8, "A1"],
+        ["Wsciekły pies (1szt.)",["Wódka"],["Syrop malinowy", "tabasco"],3, "A2"],
+        ["Opening (2szt.)",["Whiskey","Słodki wermut"],["Grenadyna"],5, "A3"],
+        ["Iguana (2szt.)",["Wódka","Srebrna tequila","likier kawowy"],["-"],8, "A4"],
+        ["Melon Ball (3szt.)",["Wódka","Likier melonowy"],["Sok pomarańczowy"],5, "A5"],
+        
+        
+        #Short Glass
+        ['Cosmopolitan',['Wódka limonkowa','triple sec'],['sok żurawinowy','sok z limonki'],7, "B1"],
+        ['Wódka Sour',['Wódka'],['sok z cytryny', 'Syrop cukrowy'], 6, "B2"],
+        ['Illusion',['Wódka', 'cointreau', 'likier melonowy'],['sok ananasowy'], 7, "B3"],
+        ['Green Eyes',['Wódka', 'likier blue curacao','likier migdałowy'],['sok pomarańczowy','sok z cytryny'], 5, "B4"],
+        ['Ruby Martini',['Wódka cytrynowa', 'Tripple sec'],["Sok Grejpfrutowy", "Syrop cukrowy"], 6, "B5"],
+        
+        
+        
+        
+        #Long Glass
+        ['Sex On The Beach',['Wódka','Likier brzoskwiniowy'],['Sok pomarańczowy','Sok Żurawinowy'],7, "C1"],
+        
+        
+        
+        #Hard Glass
+        ['Long Island Iced Tea',['Wódka','Gin', "Jasny rum","Srebrna tequila","Cointreau"],["Sok z limonki","Sok z cytryny","Syrop cukrowy","Cola"],13, "D1"],
+        
+        
+        ]
+    return menu
